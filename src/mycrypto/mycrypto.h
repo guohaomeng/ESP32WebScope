43406e220d3e1749f0848d4d1e37d82582d83136
guohaomeng <3020721699@qq.com>
2022-07-07 17:00:26 +0800

    update websocket libiary

--- src/mycrypto/mycrypto.h
+++ src/mycrypto/mycrypto.h
@@ -19,12 +19,11 @@
 #include "soc/dport_access.h"
 #include "soc/hwcrypto_reg.h"
 
+#if ESP_IDF_VERSION_MAJOR >= 4
+#include "esp_private/periph_ctrl.h"
+#else
 #include "driver/periph_ctrl.h"
-// #if ESP_IDF_VERSION_MAJOR >= 4
-// #include "esp_private/periph_ctrl.h"
-// #else
-// #include "driver/periph_ctrl.h"
-// #endif
+#endif
 
 namespace mycrypto
 {
